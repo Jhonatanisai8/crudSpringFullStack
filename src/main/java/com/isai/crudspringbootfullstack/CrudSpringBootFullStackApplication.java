8c55dee8c7cfd8cade851148f26cf9bcfcf5763c
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-26 11:47:34 -0500

    first commit

---
+++ src/main/java/com/isai/crudspringbootfullstack/CrudSpringBootFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.isai.crudspringbootfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSpringBootFullStackApplication.class, args);
+    }
+
+}
